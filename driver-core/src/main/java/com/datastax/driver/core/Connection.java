f9f6dc10662dafb8feafea4287be5ea1b9155529
Piotr Jastrzebski <haaawk@gmail.com>
2019-04-19 11:23:16 +0200

    Add shardId field to Connection
    This field will be set during initialization and will
    describe the shard in the host that a connection is associated to.
    
    Signed-off-by: Piotr Jastrzebski <haaawk@gmail.com>

--- driver-core/src/main/java/com/datastax/driver/core/Connection.java
+++ driver-core/src/main/java/com/datastax/driver/core/Connection.java
@@ -113,6 +113,7 @@ class Connection {
   private final Host host;
   final InetSocketAddress address;
   private final String name;
+  private volatile Integer shardId = null;
 
   @VisibleForTesting volatile Channel channel;
   private final Factory factory;
@@ -801,6 +802,10 @@ class Connection {
   /** @return whether the connection was already associated with an owner */
   boolean setOwner(Owner owner) {
     return ownerRef.compareAndSet(null, owner);
+  }
+
+  public int shardId() {
+    return shardId == null ? 0 : shardId;
   }
 
   /**
