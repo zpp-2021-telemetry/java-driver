23b8b44349536fa8a0cfc22659a35b8f01a1486d
Piotr Jastrzebski <haaawk@gmail.com>
2019-04-19 10:24:22 +0200

    Add Host field to Connection
    Part of Connection initialization will be obtaining
    info about shards in the host so Connection needs
    a reference to the host to be able to set up this info.
    
    Signed-off-by: Piotr Jastrzebski <haaawk@gmail.com>

--- driver-core/src/main/java/com/datastax/driver/core/Connection.java
+++ driver-core/src/main/java/com/datastax/driver/core/Connection.java
@@ -110,6 +110,7 @@ class Connection {
 
   volatile long maxIdleTime;
 
+  private final Host host;
   final InetSocketAddress address;
   private final String name;
 
@@ -144,8 +145,9 @@ class Connection {
    * @param owner the component owning this connection (may be null). Note that an existing
    *     connection can also be associated to an owner later with {@link #setOwner(Owner)}.
    */
-  protected Connection(String name, InetSocketAddress address, Factory factory, Owner owner) {
-    this.address = address;
+  protected Connection(String name, Host host, Factory factory, Owner owner) {
+    this.host = host;
+    this.address = host.getSocketAddress();
     this.factory = factory;
     this.dispatcher = new Dispatcher();
     this.name = name;
@@ -156,8 +158,8 @@ class Connection {
   }
 
   /** Create a new connection to a Cassandra node. */
-  Connection(String name, InetSocketAddress address, Factory factory) {
-    this(name, address, factory, null);
+  Connection(String name, Host host, Factory factory) {
+    this(name, host, factory, null);
   }
 
   ListenableFuture<Void> initAsync() {
@@ -955,7 +957,7 @@ class Connection {
       if (isShutdown) throw new ConnectionException(address, "Connection factory is shut down");
 
       host.convictionPolicy.signalConnectionsOpening(1);
-      Connection connection = new Connection(buildConnectionName(host), address, this);
+      Connection connection = new Connection(buildConnectionName(host), host, this);
       // This method opens the connection synchronously, so wait until it's initialized
       try {
         connection.initAsync().get();
@@ -970,8 +972,7 @@ class Connection {
         throws ConnectionException, InterruptedException, UnsupportedProtocolVersionException,
             ClusterNameMismatchException {
       pool.host.convictionPolicy.signalConnectionsOpening(1);
-      Connection connection =
-          new Connection(buildConnectionName(pool.host), pool.host.getSocketAddress(), this, pool);
+      Connection connection = new Connection(buildConnectionName(pool.host), pool.host, this, pool);
       try {
         connection.initAsync().get();
         return connection;
@@ -988,9 +989,7 @@ class Connection {
       pool.host.convictionPolicy.signalConnectionsOpening(count);
       List<Connection> connections = Lists.newArrayListWithCapacity(count);
       for (int i = 0; i < count; i++)
-        connections.add(
-            new Connection(
-                buildConnectionName(pool.host), pool.host.getSocketAddress(), this, pool));
+        connections.add(new Connection(buildConnectionName(pool.host), pool.host, this, pool));
       return connections;
     }
 
