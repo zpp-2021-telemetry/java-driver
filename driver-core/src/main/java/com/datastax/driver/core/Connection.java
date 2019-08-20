77c56ebd4ee2215f16685138a2c64b9b5ed27f25
Piotr Jastrzebski <piotr@scylladb.com>
2019-08-20 16:08:20 +0200

    Update sharding info when node restarts
    Fixes #14
    
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- driver-core/src/main/java/com/datastax/driver/core/Connection.java
+++ driver-core/src/main/java/com/datastax/driver/core/Connection.java
@@ -442,6 +442,9 @@ class Connection {
             if (sharding != null) {
               host.setShardingInfo(sharding.shardingInfo);
               Connection.this.shardId = sharding.shardId;
+            } else {
+              host.setShardingInfo(null);
+              Connection.this.shardId = 0;
             }
             return MoreFutures.VOID_SUCCESS;
           default:
@@ -902,7 +905,7 @@ class Connection {
   }
 
   public int shardId() {
-    return shardId == null ? 0 : shardId;
+    return shardId == null || host.getShardingInfo() == null ? 0 : shardId;
   }
 
   /**
