cf9bb0f64736696f74ce0be07fec3bd44761c77c
Laura Novich <36125151+lauranovich@users.noreply.github.com>
2020-11-18 10:34:44 +0200

    Update conf.py
    copyright not correct, changed to 2020

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = 'Scylla Java Driver'
-copyright = u'2012, ScyllaDB. All rights reserved.'
+copyright = u'2020, ScyllaDB. All rights reserved.'
 author = u'Scylla Project Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
